b7c52c74a55dfe3a7cd284855cc4cdf8f1f04c7d
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2020-08-23 17:51:43 +0200

    moved from log to plog

--- cmd/db_connection_test/main.go
+++ cmd/db_connection_test/main.go
@@ -3,18 +3,17 @@ package main
 import (
 	"context"
 	"io/ioutil"
-	"log"
 	"os"
 
+	"github.com/Sheerley/pluggabl/pkg/plog"
 	"github.com/jackc/pgx/v4"
 )
 
 func main() {
 
-
 	conn, err := pgx.Connect(context.Background(), "postgresql://localhost/database?user=user&password=password")
 	if err != nil {
-		log.Fatalf("Unable to connect to database: %v\n", err)
+		plog.Fatalf(1, "Unable to connect to database: %v\n", err)
 	}
 	defer conn.Close(context.Background())
 
@@ -23,7 +22,7 @@ func main() {
 
 	err = conn.QueryRow(context.Background(), "SELECT blob_data, blob_type FROM blobs WHERE blob_id=$1", 1).Scan(&blobData, &blobType)
 	if err != nil {
-		log.Fatalf("QueryRow failed: %v\n", err)
+		plog.Fatalf(1, "QueryRow failed: %v\n", err)
 	}
 
 	permissions := 0666
@@ -36,6 +35,6 @@ func main() {
 
 	err = ioutil.WriteFile("fetched/example"+extension, blobData, os.FileMode(permissions))
 	if err != nil {
-		log.Fatalf("Something went wrong while saving file: %v\n", err)
+		plog.Fatalf(1, "Something went wrong while saving file: %v\n", err)
 	}
 }
