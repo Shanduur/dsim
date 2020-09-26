6016f001c76407e0444784cbf5a6d26f6ed21cdd
shanduur <mateusz.urbanek.98@gmail.com>
2020-09-26 16:12:00 +0000

    feat: client and server

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,9 +1,48 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/Sheerley/pluggabl/pkg/transfer"
+
+	"github.com/Sheerley/pluggabl/internal/codes"
+	"github.com/Sheerley/pluggabl/internal/convo"
+	"github.com/Sheerley/pluggabl/pkg/pb"
+	"github.com/Sheerley/pluggabl/pkg/plog"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
 )
 
 func main() {
-	fmt.Println("hello world")
+	conf, err := convo.LoadConfiguration("config/config_client.json")
+	if err != nil {
+		plog.Fatalf(codes.ConfError, "error while loading configuration: %v", err)
+	}
+
+	address := fmt.Sprintf("%v:%v", conf.ManagerAddress, conf.ManagerPort)
+
+	plog.Messagef("dial server %v", address)
+
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		plog.Fatalf(codes.ClientConnectionError, "cannot dial server: %v", err)
+	}
+
+	umClient := pb.NewUserServiceClient(conn)
+
+	creds := transfer.NewDummyCredentials()
+	req := &pb.ActionUserRequest{
+		Credentials: creds,
+	}
+
+	res, err := umClient.CreateUser(context.Background(), req)
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok && int(st.Code()) == int(pb.Response_error) {
+			plog.Fatalf(codes.DbError, "%v %v", err)
+		}
+	}
+
+	plog.Messagef("created user: %v", res.Response.ReturnCode)
 }
