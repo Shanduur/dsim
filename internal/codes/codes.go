ca94f505f5e808bf3c297c7b052cabefd3944d73
shanduur <mateusz.urbanek.98@gmail.com>
2020-09-26 17:13:43 +0000

    feat: added first checkable error

--- internal/codes/codes.go
+++ internal/codes/codes.go
@@ -1,3 +1,4 @@
 // Package codes specifies all kind of constants that are used in the
-// pluggabl apps. It also specifies additional errors.
+// pluggabl apps. It also specifies additional errors, defined for situations
+// when checking errors is necessary before proceeding.
 package codes
