f482dda2338d4ab650cb3eb0af4780ab664d58b8
shanduur <echelon.div@gmail.com>
2020-10-11 16:34:42 +0000

    feat: added auth func and insert for blob

--- pkg/db/crud.go
+++ pkg/db/crud.go
@@ -9,6 +9,68 @@ import (
 	"github.com/Sheerley/pluggabl/pkg/pb"
 )
 
+// UploadFiles function inserts blobs into database
+func UploadFiles(ctx context.Context, data [][]byte, fileExtension string, user *pb.Credentials) error {
+	conn, err := connect()
+	if err != nil {
+		return fmt.Errorf("unable to connect to database: %v", err)
+	}
+
+	tx, err := conn.Begin(context.Background())
+	if err != nil {
+		return err
+	}
+
+	// in case of returning error rollback unfinished transaction
+	defer tx.Rollback(context.Background())
+
+	count := 0
+
+	err = conn.QueryRow(context.Background(), "SELECT COUNT(type_id) FROM filetypes WHERE type_extension = $1", fileExtension).Scan(&count)
+	if err != nil {
+		return fmt.Errorf("unable to execute querry: %v", err)
+	}
+
+	typeID := 0
+	if count >= 1 {
+		err = conn.QueryRow(context.Background(), "SELECT FIRST type_id FROM filetypes WHERE type_extension = $1 LIMIT 1", fileExtension).Scan(&typeID)
+		if err != nil {
+			return fmt.Errorf("unable to execute querry: %v", err)
+		}
+	}
+
+	ownerID := 0
+	err = conn.QueryRow(context.Background(), "SELECT user_id FROM users WHERE user_name = $1", user.UserId).Scan(&ownerID)
+	if err != nil {
+		return fmt.Errorf("unable to execute querry: %v", err)
+	}
+
+	for i := 0; i <= len(data); i++ {
+		_, err = tx.Exec(context.Background(),
+			"INSERT INTO blobs(blob_data, blob_type, blob_name, owner_id, insertion_date)"+
+				"VALUES ($1, $2, $3, $4, NOW())",
+			data[i],
+			typeID,
+			fmt.Sprint(i),
+			ownerID)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	if ctx.Err() == context.Canceled {
+		return &codes.SignalCanceled{}
+	}
+
+	err = tx.Commit(context.Background())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // UserExists checks if user exists inside database
 func UserExists(user *pb.Credentials) error {
 	conn, err := connect()
@@ -18,7 +80,7 @@ func UserExists(user *pb.Credentials) error {
 
 	count := 0
 
-	err = conn.QueryRow(context.Background(), "SELECT COUNT(user_id) FROM users WHERE user_name =  $1", user.UserId).Scan(&count)
+	err = conn.QueryRow(context.Background(), "SELECT COUNT(user_id) FROM users WHERE user_name = $1", user.UserId).Scan(&count)
 	if err != nil {
 		return fmt.Errorf("unable to execute querry: %v", err)
 	}
