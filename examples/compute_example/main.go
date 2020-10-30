24d383c3ea950b316736a92f7b206dfb53d5ae8f
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2020-10-30 17:04:45 +0100

    chore(compute): example cleanup

--- examples/compute_example/main.go
+++ examples/compute_example/main.go
@@ -11,9 +11,12 @@ import (
 func main() {
 	//compute.Videostream()
 	//fmt.Println(gocv.Version(), gocv.OpenCVVersion())
-	res := compute.BestMatches("/home/shanduur/Pictures/osd1.jpg", "/home/shanduur/Pictures/osd.jpg")
+	res := compute.BestMatches(
+		"/home/shanduur/repos/pluggabl/examples/images/box.png",
+		"/home/shanduur/repos/pluggabl/examples/images/box_in_scene.png")
 
 	if res.Rows() > 0 && res.Cols() > 0 {
+		plog.Messagef("git gud")
 		os.Exit(0)
 	}
 	plog.Fatalf(codes.ComputeError, "improper size of res matrix")
