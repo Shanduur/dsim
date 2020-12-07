472f053a844211883168ccf37f2547704dd3027f
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2020-12-07 18:37:15 +0100

    feat: split script for load testing

--- test/load_test.sh
+++ test/load_test.sh
@@ -7,7 +7,7 @@ else
     exit 1
 fi
 
-OPTIONS="-o=./out/$(date '+%s') -uname=user -pwd=password -log-level=5"
+OPTIONS="-uname=user -pwd=password -log-level=1"
 
 [ -d ./out/ ] && echo ok || mkdir ./out/
 rm -rf ./out/*
@@ -20,9 +20,14 @@ rm -rf ./err/*
 
 for s1 in $(ls $PHOTOFOLDER | sort -R); do
     for s2 in $(ls $PHOTOFOLDER | sort -R); do
+        echo $s1 $s2
+
         { time ./client.run \
             -source-img1=$PHOTOFOLDER/$s1 \
             -source-img2=$PHOTOFOLDER/$s2 \
+            -o=./out/$s1$s2 \
             $OPTIONS >> ./err/err.txt 2>&1; } 2>> ./log/time.txt
+
+        echo " --- " >> ./err/err.txt
     done
 done
